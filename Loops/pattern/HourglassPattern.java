cb8275a2df370e4615bc85e5062268515a37e8a4
B.RAMPRASADREDDY <bodiramprasadreddy@gmail.com>
2026-04-19 17:13:35 +0530

    HourglassPattern problem completd second approach

--- Loops/pattern/HourglassPattern.java
+++ Loops/pattern/HourglassPattern.java
@@ -1,35 +1,35 @@
 import java.util.Scanner;
 //first approach
-public class HourglassPattern {
-    public static void main(String[] args){
-      Scanner sc = new Scanner(System.in);
-      System.out.print("Enter the number :");
-      int num = sc.nextInt();
-      for(int i=0;i<num;i++){
+// public class HourglassPattern {
+//     public static void main(String[] args){
+//       Scanner sc = new Scanner(System.in);
+//       System.out.print("Enter the number :");
+//       int num = sc.nextInt();
+//       for(int i=0;i<num;i++){
 
-        for(int k=0;k<i;k++){
-            System.out.print("  ");
-        }
-        for(int j=0;j<((num-i)*2)-1;j++){
-            System.out.print(j+1+" ");
-        }
-        System.out.println("");
-      }
+//         for(int k=0;k<i;k++){
+//             System.out.print("  ");
+//         }
+//         for(int j=0;j<((num-i)*2)-1;j++){
+//             System.out.print(j+1+" ");
+//         }
+//         System.out.println("");
+//       }
 
-      //second loop
+//       //second loop
 
-    for(int i=0;i<num-1;i++){
+//     for(int i=0;i<num-1;i++){
 
-        for(int k=0;k<(num-i-1)-1;k++){
-            System.out.print("  ");
-        }
-        for(int j=0;j<=((i+1)*2);j++){
-            System.out.print(j+1+" ");
-        }
-        System.out.println("");
-    }
-    }
-}
+//         for(int k=0;k<(num-i-1)-1;k++){
+//             System.out.print("  ");
+//         }
+//         for(int j=0;j<=((i+1)*2);j++){
+//             System.out.print(j+1+" ");
+//         }
+//         System.out.println("");
+//     }
+//     }
+// }
 
 // output:-Enter the number :5
 // 1 2 3 4 5 6 7 8 9 
@@ -41,3 +41,38 @@ public class HourglassPattern {
 //     1 2 3 4 5 
 //   1 2 3 4 5 6 7 
 // 1 2 3 4 5 6 7 8 9
+
+
+
+
+//second approach
+
+public class HourglassPattern {
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the number :");
+      int num = sc.nextInt();
+      for(int i=0;i<(num*2)-1;i++){
+        if(i<num){
+        for(int k=0;k<i;k++){
+            System.out.print("  ");
+        }
+        }else{
+        for(int k=0;k<(num-(i-num))-2;k++){
+            System.out.print("  ");
+        }
+        }
+        if(i<num){
+        for(int j=0;j<((num-i)*2)-1;j++){
+            System.out.print(j+1+" ");
+        }
+        }else{
+        for(int j=0;j<((i-num+1)*2)+1;j++){
+            System.out.print(j+1+" ");
+        }
+        }
+        System.out.println("");
+      }
+
+    }
+}
