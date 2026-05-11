b295f4a38d0e2728ba88869c0284a1a293e00142
B.RAMPRASADREDDY <bodiramprasadreddy@gmail.com>
2026-05-11 19:18:04 +0530

    factorial problem completed

--- Recursion/factorial.java
+++ Recursion/factorial.java
@@ -2,7 +2,7 @@ public class factorial {
     public static void main(String[] args) {
         int n = 5;
         int result = factorial(n);
-        System.out.println("Factorial of " + n + " is: " + result);
+        System.out.println("Factorial of " + n + " is: " + result );
     }
     public static int factorial(int n){
         if(n==0){
