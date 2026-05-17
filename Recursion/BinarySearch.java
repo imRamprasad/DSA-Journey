f98b82260c56b74d94a9c58049dac4be55152a15
B.RAMPRASADREDDY <bodiramprasadreddy@gmail.com>
2026-05-18 02:04:30 +0530

    all recursion related prolems

--- Recursion/BinarySearch.java
+++ Recursion/BinarySearch.java
@@ -1,7 +1,7 @@
 public class BinarySearch {
     public static void main(String[] args){
-        int[] arr={2,2,3,4,5,6,7};
-        System.out.println(binarySearch(arr,0,arr.length-1,1));
+        int[] arr={1,2,3,4,5,6,7};
+        System.out.println(binarySearch(arr,0,arr.length-1,30));
     }
     public static int binarySearch(int[] arr,int start,int end ,int val){
         int mid=start+(end-start)/2;
@@ -12,7 +12,7 @@ public class BinarySearch {
         if(arr[mid]>val){
             return binarySearch(arr, start,mid-1, val);
         }else if(arr[mid]<val){
-            return binarySearch(arr, mid+1,end, val);
+            return binarySearch(arr, mid,end, val);
         }else if(arr[mid]==val){
             return mid;
         }else{
